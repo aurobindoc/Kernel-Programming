731515f862d001eb00d5e4071d18be7c98cf69aa
Aurobindo Mondal <aurobindoc1991@gmail.com>
2016-01-28 18:18:37 +0530

    Added interceptor.c

--- Assignment-4/interceptor.c
+++ Assignment-4/interceptor.c
@@ -2,12 +2,14 @@
 #include <linux/kernel.h>
 #include <linux/syscalls.h>
 
-unsigned long **sys_call_table;
-
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Aurobindo Mondal");
 
-unsigned long **acquire_sys_call_table(void) {
+asmlinkage long (* ref_sys_pinformation)(void);
+unsigned long **sys_call_table;
+unsigned long original_cr0;
+
+unsigned long **get_sys_call_table(void) {
 	unsigned long offset = PAGE_OFFSET;
 	unsigned long **sct;
 	
@@ -20,16 +22,36 @@ unsigned long **acquire_sys_call_table(void) {
 	return NULL;
 }
 
-int __init interceptor_init(void) {
-	if(!(sys_call_table = get_sys_call_table()))	return -1;
+asmlinkage long new_sys_pinformation(void)
+{
+        struct task_struct *tsk = current;
+        printk(KERN_INFO "This is intercepted system call. It returns the parent process ID.\n");
+        printk(KERN_INFO "pid = %d and ppid = %d \n",tsk->pid,tsk->parent->pid);
+        return tsk->parent->pid;
+}
 
-	
+int __init interceptor_init(void) 
+{
+	if(!(sys_call_table = get_sys_call_table()))	
+		return -1;
+
+	original_cr0 = read_cr0();
+	write_cr0(original_cr0 & ~0x00010000);
+	ref_sys_pinformation = (void *)sys_call_table[__NR_pinformation];
+	sys_call_table[__NR_pinformation] = (unsigned long *)new_sys_pinformation;
+	write_cr0(original_cr0);
+
 	return 0;
 }
 
-void __exit interceptor_end(void) {
+void __exit interceptor_end(void) 
+{
+	if(!sys_call_table)	return;
 
+	write_cr0(original_cr0 & ~0x00010000);
+	sys_call_table[__NR_pinformation] = (unsigned long *)ref_sys_pinformation;
+	write_cr0(original_cr0);	
 }
 
-module_init(interceptor_start);
+module_init(interceptor_init);
 module_exit(interceptor_end);
