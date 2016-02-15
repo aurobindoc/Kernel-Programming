4b6e7fd97753f568feecbfd0f6f7a275ecfbc2cb
Aurobindo Mondal <aurobindoc1991@gmail.com>
2016-02-15 20:22:27 +0530

    Assignment 6: Completed

--- Assignment-6/q.c
+++ Assignment-6/q.c
@@ -9,13 +9,15 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Aurobindo Mondal");
 
+DECLARE_KFIFO_PTR(fifo, int);	/* Declare a pointer to Queue */
 
-
-//DECLARE_KFIFO(fifo, unsigned int, PAGE_SIZE);	/* Declare a pointer to Queue */
+//DECLARE_KFIFO(fifo, unsigned int, PAGE_SIZE);	
 //INIT_KFIFO (fifo) ;
 
 
 //struct kfifo fifo;
+
+DECLARE_KFIFO(test, unsigned char, PAGE_SIZE);
 
 int numberOfProcess(void)	{
 	int count=0;
@@ -28,8 +30,9 @@ int numberOfProcess(void)	{
 
 int producer(void)	{
 	struct task_struct *task;
-	//if(kfifo_alloc(&fifo, sizeof(int)*numberOfProcess(), GFP_KERNEL)) return 0;
+	if(kfifo_alloc(&fifo, sizeof(int)*numberOfProcess(), GFP_KERNEL)) return 0;
 	for_each_process(task)	{
+		printk(KERN_ALERT "%d\n", task->pid);
 		if(!kfifo_put(&fifo, task->pid)) return 1;
 	}
 	return 1;
@@ -37,24 +40,43 @@ int producer(void)	{
 
 void consumer(void)	{
 	int val;
-	while(kfifo_get(&fifo, val)) {
+	while(kfifo_get(&fifo, &val)) {
 		printk(KERN_ALERT "pid = %d\n", val);
 	}
 }
 
 int init_module(void) {
+	int i;
+	unsigned char res;
+	printk(KERN_ALERT "Hey How're you???\n");
 	struct task_struct *task;
-	if(producer())	return -1;
+	if(!producer())	return -1;
 	consumer();
 	printk(KERN_ALERT "\n-----------------------------\n");
 	for_each_process(task)	printk(KERN_ALERT "[test] pid = %d\n", task->pid);
+	printk(KERN_ALERT "\n-----------------------------\n Putting different types of variable in queue: \n");
+	INIT_KFIFO(test);
+	
+	kfifo_in(&test, "hello", 5);
+	
+	for(i=0; i!=10; i++)	kfifo_put(&test, i);
+	
+	kfifo_put(&test, 'a');
+	kfifo_put(&test, 'u');
+	kfifo_put(&test, 'r');
+	kfifo_put(&test, 'o');
+	
+	while(kfifo_get(&test, &res))	{
+		printk(KERN_ALERT "%c\n",res);
+	}
+	
 	 	
 	return 0;
 }
 	
 void cleanup_module(void) {
 	kfifo_free(&fifo);
-	printk(KERN_INFO "It's done!!!!");
+	printk(KERN_ALERT "It's done!!!!");
 }
 	
 
